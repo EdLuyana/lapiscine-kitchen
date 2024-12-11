0b11821c225736624db8ee645c1ef5d0b59c52d3
Ed <edouard.duron@lapiscine.pro>
2024-12-11 16:17:10 +0100

    CRUD V1

--- src/Controller/Admin/AdminRecipeController.php
+++ src/Controller/Admin/AdminRecipeController.php
@@ -15,7 +15,7 @@ class AdminRecipeController extends AbstractController
 {
 
 
-    #[Route('/admin/recipe/create', 'admin_create_recipe', methods: ['POST','GET'])]
+    #[Route('/admin/recipe/create', 'admin_create_recipe', methods: ['POST', 'GET'])]
     public function createRecipe(Request $request, EntityManagerInterface $entityManager, ParameterBagInterface $parameterBag)
     {
         $recipe = new Recipe();
@@ -54,15 +54,18 @@ class AdminRecipeController extends AbstractController
     }
 
     #[Route('/admin/recipes/list', 'admin_list_recipes', methods: ['GET'])]
-public function listRecipes(RecipeRepository $recipeRepository){
+    public function listRecipes(RecipeRepository $recipeRepository)
+    {
         $recipes = $recipeRepository->findAll();
 
         return $this->render('admin/recipe/list_recipes.html.twig', [
             'recipes' => $recipes
         ]);
     }
-    #[Route('/admin/recipes/{id}/delete', 'admin_delete_recipe', methods: ['POST', 'GET'], requirements: ['id'=>'\d+'])]
-public function deleteRecipe(int $id, RecipeRepository $recipeRepository, EntityManagerInterface $entityManager){
+
+    #[Route('/admin/recipes/{id}/delete', 'admin_delete_recipe', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function deleteRecipe(int $id, RecipeRepository $recipeRepository, EntityManagerInterface $entityManager)
+    {
 
         $recipe = $recipeRepository->find($id);
 
@@ -72,5 +75,44 @@ public function deleteRecipe(int $id, RecipeRepository $recipeRepository, Entity
         $this->addFlash('success', 'La recette a bien été envoyée dans la poubelle des recettes foireuses');
 
         return $this->redirectToRoute('admin_list_recipes');
+    }
+
+    #[Route('/admin/recipe/{id}/update', 'admin_update_recipe', methods: ['POST', 'GET'], requirements: ['id' => '\d+'])]
+    public function updateRecipe(int $id, RecipeRepository $recipeRepository, EntityManagerInterface $entityManager, Request $request, ParameterBagInterface $parameterBag)
+    {
+        $recipe = $recipeRepository->find($id);
+        $adminRecipeForm = $this->createForm(AdminRecipeType::class, $recipe);
+
+        $adminRecipeForm->handleRequest($request);
+
+        if ($adminRecipeForm->isSubmitted()) {
+
+            $recipeImage = $adminRecipeForm->get('image')->getData();
+
+            if ($recipeImage) {
+
+                $imageNewName = uniqid() . '.' . $recipeImage->guessExtension();
+
+                $rootDir = $parameterBag->get('kernel.project_dir');
+
+                $uploadsDir = $rootDir . '/public/assets/uploads';
+
+                $recipeImage->move($uploadsDir, $imageNewName);
+
+                $recipe->setImage($imageNewName);
+            }
+
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+            $this->addFlash('success', 'Modification recette confirmée');
+        }
+
+        $formView = $adminRecipeForm->createView();
+
+        return $this->render('admin/recipe/update_recipe.html.twig', [
+            'formView' => $formView,
+            'recipe' => $recipe
+        ]);
+
     }
 }
