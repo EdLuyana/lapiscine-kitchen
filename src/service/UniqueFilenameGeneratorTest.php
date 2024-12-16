7abfb8274899b9bab5e6335aa948ebd438b51b6a
Ed <edouard.duron@lapiscine.pro>
2024-12-16 14:04:56 +0100

    test unitaire

---
+++ src/service/UniqueFilenameGeneratorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\service;
+
+use PHPUnit\Framework\TestCase;
+
+class UniqueFilenameGeneratorTest extends TestCase
+{
+
+    public function testGenerateUniqueFilename()
+    {
+        $uniqueFileNameGenerator = new UniqueFilenameGenerator();
+        $uniqueFilename = $uniqueFileNameGenerator->generateUniqueFilename('hello','jpeg');
+
+        $this->assertFileExists('jpeg', $uniqueFilename);
+
+    }
+
+}
